78642eeef2100a2f191578eb83db1767f8a531f2
jonathan.brayette@gmail.com <jonathan.brayette@gmail.com>
2015-04-15 11:13:22 +0200

    Added empty promotions generation + switch to yml config for services

--- src/Cac/BarBundle/Services/PromotionManager.php
+++ src/Cac/BarBundle/Services/PromotionManager.php
@@ -9,17 +9,69 @@ use Cac\BarBundle\Entity\PromotionOptionCategory;
 class PromotionManager
 {
     private $days;
-    private $restriction;
     private $em;
+    private $restriction;
+    private $value;
+    private $quantity;
+    private $emptyPromotions;
 
     public function __construct(EntityManager $entityManager) {
+        $this->days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
         $this->em = $entityManager;
+        $this->restriction = $em->getRepository('CacBarBundle:PromotionOptionCategory')->findOneByShortcode('restriction');
+        $this->value = $em->getRepository('CacBarBundle:PromotionOptionCategory')->findOneByShortcode('value');
+        $this->quantity = $em->getRepository('CacBarBundle:PromotionOptionCategory')->findOneByShortcode('quantity');
+        $this->emptyPromotions = new \Doctrine\Common\Collections\ArrayCollection();
+
+        $this->generateEmptyPromotions();
     }
 
     public function generateEmptyPromotions()
     {
+        foreach($this->days as $day) {
+            $promotion = new Promotion();
+            $promotion->setDay($day);
+            $promotion->setCategory('promotion');
 
+            $restrictionOption = new PromotionOption();
+            $restrictionOption->setCategory($this->restriction);
+            $restrictionOption->setValue('');
+            $restrictionOption->setName('');
 
-        return array();
+            $valueOption = new PromotionOption();
+            $valueOption->setCategory($this->value);
+            $valueOption->setValue('');
+            $valueOption->setName('');
+
+            $quantityOption = new PromotionOption();
+            $quantityOption->setCategory($this->quantity);
+            $quantityOption->setValue('');
+            $quantityOption->setName('');
+
+            $promotion->addOption($restrictionOption);
+            $promotion->addOption($valueOption);
+            $promotion->addOption($quantityOption);
+
+            $this->addEmptyPromotion($promotion);
+        }
+
+        return $this;
+    }
+
+    public function populatePromotion(\Cac\BarBundle\Entity\Promotion $promotion)
+    {
+        return $this;
+    }
+
+    public function getEmptyPromotions()
+    {
+        return $this->emptyPromotions;
+    }
+
+    public function addEmptyPromotion(\Cac\BarBundle\Entity\Promotion $promotion)
+    {
+        $this->promotions[] = $promotion;
+
+        return $this;
     }
 }
