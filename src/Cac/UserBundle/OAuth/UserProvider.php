5121bffd6f721b2e53fef1aaf12f35b5dd073792
jonathan.brayette@gmail.com <jonathan.brayette@gmail.com>
2015-05-31 17:25:42 +0200

    Fuck it, I'm becomming a drug dealer !

--- src/Cac/UserBundle/OAuth/UserProvider.php
+++ src/Cac/UserBundle/OAuth/UserProvider.php
@@ -11,6 +11,7 @@ use Cac\UserBundle\Entity\User;
 
 use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\DependencyInjection\Container;
 
 /**
  * Loading and ad-hoc creation of a user by an OAuth sign-in provider account.
@@ -22,12 +23,14 @@ class UserProvider extends FOSUBUserProvider
     protected $userManager;
     protected $properties;
     protected $discriminator;
+    protected $container;
 
-    public function __construct(\FOS\UserBundle\Model\UserManagerInterface $userManager, array $properties, \PUGX\MultiUserBundle\Model\UserDiscriminator $discriminator)
+    public function __construct(\FOS\UserBundle\Model\UserManagerInterface $userManager, array $properties, \PUGX\MultiUserBundle\Model\UserDiscriminator $discriminator, Container $container)
     {
         $this->userManager = $userManager;
         $this->properties = $properties;
         $this->discriminator = $discriminator;
+        $this->container = $container;
     }
 
     /**
@@ -70,20 +73,40 @@ class UserProvider extends FOSUBUserProvider
         $this->discriminator->setClass('Cac\UserBundle\Entity\BasicUser');
         $user = $this->userManager->createUser();
         $this->updateUserByOAuthUserResponse($user, $response);
+        $em = $this->container->get('doctrine')->getManager();
 
-        // set default values taken from OAuth sign-in provider account
-        $this->discriminator->setClass('Cac\UserBundle\Entity\User');
-        if (null !== $email = $response->getEmail()) {
-            var_dump($this->userManager->findUserBy(array('email' => $response->getEmail())));
-            if(null === $this->userManager->findUserByEmail($response->getEmail())) {
-                $user->setEmail($email);
-            } else {
-
-            }
+        $tmpUser = $em->getRepository('Cac\UserBundle\Entity\User')->findOneByEmail($response->getEmail());
+        if($tmpUser !== null) {
+            $basicUser = $em->getRepository('Cac\UserBundle\Entity\BasicUser')->findOneById($tmpUser->getId());
+            $bigboss = $em->getRepository('Cac\UserBundle\Entity\Bigboss')->findOneById($tmpUser->getId());
         }
 
-        var_dump($response);
+        var_dump($tmpUser);
         die;
+
+        // set default values taken from OAuth sign-in provider account
+        if (null !== $email = $response->getEmail()) {
+            if(null !== $basicUser || null !== $bigboss) {
+                if(null !== $basicUser) {
+                    $user->setFacebookId($response->getId());
+                    $logUser = $basicUser;
+                } else {
+                    $logUser = $bigboss;
+                }
+
+                $logToken = new \Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken($logUser, null, "main", array("ROLE_USER"));
+                $this->get('security.context')->setToken($logToken);
+
+                $event = new \Symfony\Component\Security\Http\Event\InteractiveLoginEvent($this->getRequest(), $logToken);
+                $this->get('event_dispatcher')->dispatch('security.interactive_login', $event);
+
+                var_dump('hey');
+                die;
+                return $this->redirect($this->generateUrl('home'));
+            } else {
+                $user->setEmail($email);
+            }
+        }
 
         if (null === $this->userManager->findUserByUsername($response->getNickname())) {
             $user->setUsername($response->getNickname());
