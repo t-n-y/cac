c1b6f4564855a5f693691cfd2d8696a1b7a37dbd
Kevin Marien Pro <kevinmarien14@gmail.com>
2015-06-23 17:01:15 +0200

    start : gestion du nombre de parrainage

--- web/js/function.js
+++ web/js/function.js
@@ -945,21 +945,47 @@ function closeConfirmationReservation(){
 var $dayOngletNewBar = $('.dayOngletNewBar');
 var numberOfDayOnglet;
 var $promoHappyHour = $('.promoHappyHour');
+var $promoParrainnage = $('.promoParrainnage');
+
+var jsonBrut = $('#form_sponsorship_json').val();
+var parseJson = JSON.parse(jsonBrut);
+
+// jQuery.each(parseJson, function(i, val) {
+//    console.log(val);
+// });
+
+
+
 
 numberOfDayOnglet = $dayOngletNewBar.length;
 
 $($dayOngletNewBar[0]).addClass('selectedOngletDay');
 $($promoHappyHour[0]).addClass('selectedPromoHappyHour');
+$($promoParrainnage[0]).addClass('selectedPromoParrainnage');
 
+//récupération nombre de parrainage du premier jour
+var nbParrainnageToday = parseJson["Lundi"];
+$('.nbParrainnageToday').html(nbParrainnageToday.number);
 
 $dayOngletNewBar.on('click', function(){
-    $dayOngletNewBar.removeClass('selectedOngletDay');
-    $(this).addClass('selectedOngletDay');
-    var daySelected = $(this).data("dayselected");
 
-    $promoHappyHour.removeClass('selectedPromoHappyHour');
-    $($promoHappyHour[(daySelected-1)]).addClass('selectedPromoHappyHour');
+  //récupération nombre de parrainage du jour selectionné
+  var daySelected = $(this).html();
+  var nbParrainnageToday = parseJson[daySelected];
+  $('.nbParrainnageToday').html(nbParrainnageToday.number);
+
+  $dayOngletNewBar.removeClass('selectedOngletDay');
+  $(this).addClass('selectedOngletDay');
+  var daySelected = $(this).data("dayselected");
+
+  $promoHappyHour.removeClass('selectedPromoHappyHour');
+  $($promoHappyHour[(daySelected-1)]).addClass('selectedPromoHappyHour');
+  $promoParrainnage.removeClass('selectedPromoParrainnage');
+  $($promoParrainnage[(daySelected-1)]).addClass('selectedPromoParrainnage');
 });
+
+
+
 
 
 $('.programmeFidelite span').on('click', function(){
@@ -1094,5 +1120,9 @@ $('#senddrink').on('click', function(){
         }
     });
 });
+
+/****GET PARRAINNAGE ****/
+
+
 
 
