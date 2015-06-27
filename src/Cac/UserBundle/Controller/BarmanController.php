eb085f8e87aa950d28511451bab4e9eb46ea400e
tiny-dev <g.leclercq12@gmail.com>
2015-06-27 21:41:06 +0200

    list barman plus method delete barman

--- src/Cac/UserBundle/Controller/BarmanController.php
+++ src/Cac/UserBundle/Controller/BarmanController.php
@@ -9,6 +9,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Cac\UserBundle\Entity\Barman;
 use Cac\UserBundle\Form\RegistrationBarmanFormType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
  * Barman controller.
@@ -28,5 +30,43 @@ class BarmanController extends Controller
         return $this->container
                     ->get('pugx_multi_user.registration_manager')
                     ->register('Cac\UserBundle\Entity\Barman');
+    }
+
+    /**
+     * list all barmans.
+     *
+     * @Route("/list/{idBigboss}", name="barman_list")
+     * @Template()
+     */
+    public function barmanListAction($idBigboss)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $bars = $em->getRepository("CacBarBundle:Bar")->findByAuthor($idBigboss);  
+        $barmans = array();
+        foreach ($bars as $bar) {
+            $nbBarman = count($bar->getBarman()->getValues());
+            for ($i=0; $i < $nbBarman ; $i++) { 
+                $barmans[] = $bar->getBarman()->getValues()[$i];
+            }
+        }    
+        return array('barmans' => $barmans);
+    }
+
+    /**
+     * delete barman.
+     *
+     * @Route("/delete/{id}", name="barman_delete", options={"expose"=true})
+     * @Template()
+     */
+    public function barmanDeleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $barman = $em->getReference('Cac\UserBundle\Entity\Barman', $id);
+        if ($barman->getBar()->getAuthor() !== $this->get('security.context')->getToken()->getUser()) {
+            throw new AccessDeniedException('Vous n\'êtes pas autorisé à effectuer cette action.');
+        }
+        $em->remove($barman);
+        $em->flush();
+        return new Response("Barman supprimé");
     }
 }
