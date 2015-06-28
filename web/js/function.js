d080fdc5e1298be237c7d44eb4c7ba152fe7363c
Kevin Marien Pro <kevinmarien14@gmail.com>
2015-06-28 13:19:57 +0200

    calendrier en français prout

--- web/js/function.js
+++ web/js/function.js
@@ -1061,6 +1061,17 @@ $('.ticketVerreValidate').on('click', function(){
             $( "#datepicker" ).datepicker({ 
                 minDate: 0, 
                 maxDate: "+6D",
+                firstDay: 1 ,
+                closeText: 'Fermer',
+                prevText: 'Précédent',
+                nextText: 'Suivant',
+                currentText: 'Aujourd\'hui',
+                monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+                monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+                dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+                dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
+                dayNamesMin: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+                weekHeader: 'Sem.',
                 beforeShowDay: function(date){
                     var string = jQuery.datepicker.formatDate('dd-mm-yy', date);
                     return [ disableddates.indexOf(string) == -1 ]
