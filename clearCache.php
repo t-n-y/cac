84b1e27827c8a622eb9fc8b4890eecd9c528d2c8
guillaume leclercq <guillaume.leclercq@fullsix.com>
2015-10-18 19:22:29 +0200

    own cache for cron

---
+++ clearCache.php
@@ -0,0 +1,23 @@
+<?php
+$str = __DIR__.'/app/cache/';
+
+
+function recursiveDelete($str){
+if(is_file($str)){
+    return @unlink($str);
+}
+elseif(is_dir($str)){
+    $scan = glob(rtrim($str,'/').'/*');
+    foreach($scan as $index=>$path){
+        recursiveDelete($path);
+    }
+    return @rmdir($str);
+}
+}
+
+recursiveDelete($str);
+
+//here you can redirect to any page
+echo(__DIR__.'/app/cache/*'.' -> Clear completed'); 
+
+?>
