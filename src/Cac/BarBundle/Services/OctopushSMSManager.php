d8a68d4b066a15cd3dc562a4fde41484ea3457dd
jonathan.brayette@gmail.com <jonathan.brayette@gmail.com>
2016-05-15 15:10:21 +0200

    Update Octopush API + params

--- src/Cac/BarBundle/Services/OctopushSMSManager.php
+++ src/Cac/BarBundle/Services/OctopushSMSManager.php
@@ -6,7 +6,15 @@ use Cac\BarBundle\Lib\Octopush\SMS;
 
 class OctopushSMSManager
 {
-    protected $config;
+    // Octopush Globals as const
+    const SMS_STANDARD = 'XXX';
+    const SMS_WORLD = 'WWW';
+    const SMS_PREMIUM = 'FR';
+    const INSTANTANE = 1;
+    const DIFFERE = 2;
+    const SIMULATION = 'simu';
+    const REEL = 'real';
+
     public $user_login; // string
     public $api_key;   // string
     public $sms_text; // string
@@ -16,8 +24,9 @@ class OctopushSMSManager
     public $sms_fields_1;  // array
     public $sms_fields_2;  // array
     public $sms_fields_3;  // array
+    public $sms_mode;  // int (instantanous or deffered)
     public $sms_type;  // int (standard or pro)
-    public $sending_time;  // int
+    public $sending_date;  // int
     public $sms_sender;   // string
     public $request_mode;   // string
     public $request_id;   // string
@@ -27,32 +36,45 @@ class OctopushSMSManager
     public $msisdn_sender; // int
     public $request_keys; // int
 
+    // SMS parts
+    public $user_batch_id; // string
+    public $finished; // int
+    public $action; // string
+
+    protected $config;
+
     public function __construct($config)
     {
         $this->config = $config;
 
-        $this->user_login	 = '';
-        $this->api_key		 = '';
-
+        $this->user_login	 = $this->config['user_login'];
+        $this->api_key		 = $this->config['api_key'];
         $this->sms_text = '';
-
         $this->sms_recipients			 = array();
         $this->recipients_first_names	 = array();
         $this->recipients_last_names	 = array();
         $this->sms_fields_1				 = array();
         $this->sms_fields_2				 = array();
         $this->sms_fields_3				 = array();
-
-        $this->sending_time	 = time();
-
-        $this->sms_sender	 = 'OneSender';
-        $this->sms_type		 = $this->config['sms_world'];
-
+        $this->sms_mode		 = self::INSTANTANE;
+        $this->sending_date	 = time();
+        $this->sms_d		 = date('d');
+        $this->sms_m		 = date('m');
+        $this->sms_h		 = date('H');
+        $this->sms_i		 = date('i');
+        $this->sms_y		 = date('Y');
+        $this->sms_sender	 = 'CampagneSMS';
+        $this->sms_type		 = self::SMS_STANDARD;
+        $this->sms_mode		 = self::INSTANTANE;
+        $this->request_mode	 = self::REEL;
         $this->request_id	 = '';
         $this->with_replies	 = 0;
         $this->transactional = 0;
         $this->msisdn_sender = 0;
         $this->request_keys	 = '';
+
+        // SMS parts
+        $this->user_batch_id = '';
     }
 
     public function send()
@@ -60,38 +82,36 @@ class OctopushSMSManager
         $domain	 = $this->config['domain'];
         $path	 = $this->config['path_sms'];
         $port	 = $this->config['port'];
-
         $data = array(
             'user_login'			 => $this->user_login,
             'api_key'				 => $this->api_key,
             'sms_text'				 => $this->sms_text,
             'sms_recipients'		 => implode(',', $this->sms_recipients),
-            // 'recipients_first_names'	 => implode(',', $this->recipients_first_names),
-            // 'recipients_last_names'	 => implode(',', $this->recipients_last_names),
-            // 'sms_fields_1'			 => implode(',', $this->sms_fields_1),
-            // 'sms_fields_2'			 => implode(',', $this->sms_fields_2),
-            // 'sms_fields_3'			 => implode(',', $this->sms_fields_3),
+            'recipients_first_names' => implode(',', $this->recipients_first_names),
+            'recipients_last_names'	 => implode(',', $this->recipients_last_names),
+            'sms_fields_1'			 => implode(',', $this->sms_fields_1),
+            'sms_fields_2'			 => implode(',', $this->sms_fields_2),
+            'sms_fields_3'			 => implode(',', $this->sms_fields_3),
+            'sms_mode'				 => $this->sms_mode,
             'sms_type'				 => $this->sms_type,
             'sms_sender'			 => $this->sms_sender,
-            // 'request_id'			 => $this->request_id,
-            // 'request_mode'			 => $this->request_mode,
-            // 'with_replies'			 => $this->with_replies,
-            // 'transactional'			 => $this->transactional,
-            // 'msisdn_sender'			 => $this->msisdn_sender
+            'request_mode'			 => $this->request_mode,
+            'request_id'			 => $this->request_id,
+            'with_replies'			 => $this->with_replies,
+            'transactional'			 => $this->transactional,
+            'msisdn_sender'			 => $this->msisdn_sender
         );
-        if ($this->sending_time > time())
+        if ($this->sms_mode == self::DIFFERE)
         {
             // GMT + 1 (Europe/Paris)
-            $data['sending_time'] = $this->sending_time;
+            $data['sending_date'] = $this->sending_date;
         }
-
-        // If needed, key must be computed
+        // Si des champs sont définis, on calcule la clé
         if ($this->request_keys !== '')
         {
             $data['request_keys']	 = $this->request_keys;
-            $data['request_sha1']	 = $this->_get_request_sha1_string($data);
+            $data['request_sha1']	 = $this->_get_request_sha1_string($this->request_keys, $data);
         }
-
         return trim($this->_httpRequest($domain, $path, $port, $data));
     }
 
@@ -100,20 +120,18 @@ class OctopushSMSManager
         $domain	 = $this->config['domain'];
         $path	 = $this->config['path_balance'];
         $port	 = $this->config['port'];
-
         $data = array(
             'user_login' => $this->user_login,
             'api_key'	 => $this->api_key
         );
-
         return trim($this->_httpRequest($domain, $path, $port, $data));
     }
-
-    private function _get_request_sha1_string($data)
+    private function _get_request_sha1_string($request_keys, $data)
     {
         $A_char_to_field = array(
             'T'	 => 'sms_text',
             'R'	 => 'sms_recipients',
+            'M'	 => 'sms_mode',
             'Y'	 => 'sms_type',
             'S'	 => 'sms_sender',
             'D'	 => 'sms_date',
@@ -130,52 +148,37 @@ class OctopushSMSManager
         for ($i = 0, $n = strlen($this->request_keys); $i < $n; $i++)
         {
             $char = $this->request_keys[$i];
-
             if (!isset($A_char_to_field[$char]) || !isset($data[$A_char_to_field[$char]]))
-            {
                 continue;
-            }
             $request_string .= $data[$A_char_to_field[$char]];
         }
         $request_sha1 = sha1($request_string);
         return $request_sha1;
     }
-
     private function _httpRequest($domain, $path, $port, $A_fields = array())
     {
-        set_time_limit(0);
+        $ssl = $domain . $path;
+        @set_time_limit(0);
         $qs	 = array();
         foreach ($A_fields as $k => $v)
         {
             $qs[] = $k . '=' . urlencode($v);
         }
-
         $request = join('&', $qs);
-
         if (function_exists('curl_init') AND $ch = curl_init(substr($domain, $this->config['cut']) . $path))
         {
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_PORT, $port);
-
             curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
             $result = curl_exec($ch);
             curl_close($ch);
-
             return $result;
         }
         else if (ini_get('allow_url_fopen'))
         {
-            $errno = $errstr = null;
             $fp = fsockopen(substr($domain, $this->config['cut']), $port, $errno, $errstr, 100);
-            if($errno != '' || $errstr != '')
-            {
-                echo 'errno : ' . $errno;
-                echo "\n";
-                echo 'errstr : ' . $errstr;
-            }
             if (!$fp)
             {
                 echo 'Unable to connect to host. Try again later.';
@@ -189,115 +192,92 @@ class OctopushSMSManager
             $header .= "Content-Length: " . strlen($request) . "\r\n";
             $header .= "Connection: close\r\n\r\n";
             $header .= "{$request}\r\n\r\n";
-
             fputs($fp, $header);
             $result = '';
             while (!feof($fp))
-            {
                 $result .= fgets($fp, 1024);
-            }
             fclose($fp);
-
-            $clear_result = substr($result, strpos($result, "\r\n\r\n") + 4);
-            return $clear_result;
+            $result = substr($result, strpos($result, "\r\n\r\n") + 4);
+            return $result;
         }
         else
         {
             die('Server does not support HTTP(S) requests.');
         }
-        return null;
+        return $response;
     }
-
     public function set_user_login($user_login)
     {
         $this->user_login = $user_login;
     }
-
     public function set_api_key($api_key)
     {
         $this->api_key = $api_key;
     }
-
     public function set_sms_text($sms_text)
     {
         $this->sms_text = $sms_text;
     }
-
     public function set_sms_type($sms_type)
     {
         $this->sms_type = $sms_type;
     }
-
     public function set_sms_recipients($sms_recipients)
     {
         $this->sms_recipients = $sms_recipients;
     }
-
     public function set_recipients_first_names($first_names)
     {
         $this->recipients_first_names = $first_names;
     }
-
     public function set_recipients_last_names($last_names)
     {
         $this->recipients_last_names = $last_names;
     }
-
     public function set_sms_fields_1($sms_fields_1)
     {
         $this->sms_fields_1 = $sms_fields_1;
     }
-
     public function set_sms_fields_2($sms_fields_2)
     {
         $this->sms_fields_2 = $sms_fields_2;
     }
-
     public function set_sms_fields_3($sms_fields_3)
     {
         $this->sms_fields_3 = $sms_fields_3;
     }
-
     public function set_sms_mode($sms_mode)
     {
         $this->sms_mode = $sms_mode;
     }
-
     public function set_sms_sender($sms_sender)
     {
         $this->sms_sender = $sms_sender;
     }
-
     public function set_date($y, $m, $d, $h, $i)
     {
-        $sms_y = intval($y);
-        $sms_d = intval($d);
-        $sms_m = intval($m);
-        $sms_h = intval($h);
-        $sms_i = intval($i);
-
-        $this->sending_time = mktime($sms_h, $sms_i, 0, $sms_m, $sms_d, $sms_y);
+        $this->sms_y = intval($y);
+        $this->sms_d = intval($d);
+        $this->sms_m = intval($m);
+        $this->sms_h = intval($h);
+        $this->sms_i = intval($i);
+        $this->sending_date = mktime($this->sms_h, $this->sms_i, 0, $this->sms_m, $this->sms_d);
     }
-
     public function set_simulation_mode()
     {
-        $this->request_mode = SIMULATION;
+        $this->request_mode = self::SIMULATION;
     }
-
     public function set_sms_ticket($sms_ticket)
     {
         $this->sms_ticket = $sms_ticket;
     }
-
     public function set_sms_request_id($request_id)
     {
         $this->request_id = preg_replace('`[^0-9a-zA-Z]*`', '', $request_id);
     }
-
     /*
-     * Notifies Octopush plateform that you want to recieve the SMS that your recipients will send back to your sending(s)
+     * Notify Octopush plateform that you want to recieve the answers that your recipients will send back to your sending(s)
      */
-
     public function set_option_with_replies($with_replies)
     {
         if (!isset($with_replies) || intval($with_replies) !== 1)
@@ -309,13 +289,11 @@ class OctopushSMSManager
             $this->with_replies = 1;
         }
     }
-
     /*
-     * Notifies Octopush that you are making a transactional sending.
+     * Notify Octopush that you are making a transactional sending.
      * With this option, sending marketing SMS is strongly forbidden, and may make your account blocked in case of abuses.
      * DO NOT USE this option if you are not sure to understand what a transactional SMS is.
      */
-
     public function set_option_transactional($transactional)
     {
         if (!isset($transactional) || intval($transactional) !== 1)
@@ -327,7 +305,6 @@ class OctopushSMSManager
             $this->transactional = 1;
         }
     }
-
     /*
      * Use a MSISDN number.
      */
@@ -335,10 +312,30 @@ class OctopushSMSManager
     {
         $this->msisdn_sender = $msisdn_sender;
     }
-
     public function set_request_keys($request_keys)
     {
         $this->request_keys = $request_keys;
     }
-}
 
+    public function set_user_batch_id($user_batch_id)
+    {
+        $this->user_batch_id = preg_replace('`[^0-9a-zA-Z]*`', '', $user_batch_id);
+    }
+    /*
+     * Finish the transmission of trames and allow platform to build campaign from package.
+     */
+    public function set_finished($finished)
+    {
+        if (intval($finished) === 1)
+            $this->finished	 = 1;
+        else
+            $this->finished	 = 0;
+    }
+    /*
+     * Send or delete a campaign.
+     */
+    public function set_action($action)
+    {
+        $this->action = $action;
+    }
+}
